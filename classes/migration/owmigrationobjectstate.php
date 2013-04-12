5546bcfc4e14a1090c35425c596be900349e906d
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2013-04-12 14:11:55 +0200

    Add object state API migration

--- classes/migration/owmigrationobjectstate.php
+++ classes/migration/owmigrationobjectstate.php
@@ -4,9 +4,15 @@ class OWMigrationObjectState extends OWMigrationBase {
 
     protected $objectStateGroupIdentifier;
     protected $objectStateGroup;
+    protected $topPriorityLanguage;
 
     public function startMigrationOn( $param ) {
         $this->objectStateGroupIdentifier = $param;
+        $objectStateGroup = eZContentObjectStateGroup::fetchByIdentifier( $param );
+        if( $objectStateGroup instanceof eZContentObjectStateGroup ) {
+            $this->objectStateGroup = $objectStateGroup;
+        }
+        $this->topPriorityLanguage = eZContentLanguage::topPriorityLanguage( );
     }
 
     public function end( ) {
@@ -15,22 +21,142 @@ class OWMigrationObjectState extends OWMigrationBase {
     }
 
     public function createIfNotExists( ) {
+        if( $this->objectStateGroup instanceof eZContentObjectStateGroup ) {
+            $this->output->notice( "Create if not exists : state group '$this->objectStateGroupIdentifier' exists, nothing to do." );
+            return;
+        }
+        $this->db->begin( );
+        $this->objectStateGroup = new eZContentObjectStateGroup( );
+        $this->objectStateGroup->setAttribute( 'identifier', $this->objectStateGroupIdentifier );
+        $this->objectStateGroup->setCurrentLanguage( $this->topPriorityLanguage->attribute( 'locale' ) );
+        $translations = $this->objectStateGroup->allTranslations( );
+        foreach( $translations as $translation ) {
+            $translation->setAttribute( 'name', sfInflector::humanize( $this->objectStateGroupIdentifier ) );
+        }
+        $this->objectStateGroup->store( );
+        $this->db->commit( );
+        $this->output->notice( "Create if not exists : state group '$this->objectStateGroupIdentifier' created." );
     }
 
-    public function addState( $identifier, $params ) {
+    public function update( $params ) {
+        if( !$this->objectStateGroup instanceof eZContentObjectStateGroup ) {
+            $this->output->error( "Update : state group '$this->objectStateGroupIdentifier' not found." );
+            return;
+        }
+        foreach( $params as $key => $value ) {
+            if( $this->objectStateGroup->hasAttribute( $key ) ) {
+                $this->objectStateGroup->setAttribute( $key, $value );
+            } else {
+                $translation = $this->objectStateGroup->translationByLocale( $key );
+                if( $translation instanceof eZContentObjectStateGroupLanguage ) {
+                    if( is_array( $value ) ) {
+                        if( isset( $value['name'] ) ) {
+                            $translation->setAttribute( 'name', $value['name'] );
+                        }
+                        if( isset( $value['description'] ) ) {
+                            $translation->setAttribute( 'description', $value['description'] );
+                        }
+                    } else {
+                        $translation->setAttribute( 'name', $value );
+                    }
+                } else {
+                    $this->output->error( "update : attribute or translation '$key' not found." );
+                }
+            }
+        }
+        $this->objectStateGroup->store( );
+        $this->output->notice( "update : state group '$this->objectStateGroupIdentifier' updated." );
+    }
 
+    public function addState( $identifier, $params = array() ) {
+        if( !$this->objectStateGroup instanceof eZContentObjectStateGroup ) {
+            $this->output->notice( "Add state : state group '$this->objectStateGroupIdentifier' nou found." );
+            return;
+        }
+        $state = $this->objectStateGroup->stateByIdentifier( $identifier );
+        if( $state instanceof eZContentObjectState ) {
+            $this->output->warning( "Add state : state '$identifier' already exists." );
+            return;
+        } else {
+            $state = $this->objectStateGroup->newState( );
+            $state->setAttribute( 'identifier', $identifier );
+            $state->setCurrentLanguage( $this->topPriorityLanguage->attribute( 'locale' ) );
+            $translations = $state->allTranslations( );
+            foreach( $translations as $translation ) {
+                $locale = $translation->language( )->attribute( 'locale' );
+                $translation->setAttribute( 'name', sfInflector::humanize( $identifier ) );
+            }
+            $state->store( );
+            if( !empty( $params ) ) {
+                $state = $this->fillStateWithParams( $state, $params );
+            }
+            $state->store( );
+            $this->output->notice( "Add state : state $identifier added." );
+        }
     }
 
     public function updateState( $identifier, $params ) {
-
+        if( !$this->objectStateGroup instanceof eZContentObjectStateGroup ) {
+            $this->output->error( "Update state : state group '$this->objectStateGroupIdentifier' not found." );
+            return;
+        }
+        $state = $this->objectStateGroup->stateByIdentifier( $identifier );
+        if( $state instanceof eZContentObjectState ) {
+            $state = $this->fillStateWithParams( $state, $params );
+            $state->store( );
+            $this->output->notice( "Update state : state '$identifier' upadted." );
+        } else {
+            $this->output->warning( "Update state : state '$identifier' not found." );
+        }
     }
 
     public function removeState( $identifier ) {
-
+        if( !$this->objectStateGroup instanceof eZContentObjectStateGroup ) {
+            $this->output->error( "Remove state : state group '$this->objectStateGroupIdentifier' not found." );
+            return;
+        }
+        $state = $this->objectStateGroup->stateByIdentifier( $identifier );
+        if( $state instanceof eZContentObjectState ) {
+            $state->remove( );
+            $this->output->notice( "Remove state : state '$identifier' removed." );
+        } else {
+            $this->output->warning( "Remove state : state '$identifier' not found." );
+            return;
+        }
     }
 
     public function removeObjectStateGroup( ) {
+        if( !$this->objectStateGroup instanceof eZContentObjectStateGroup ) {
+            $this->output->error( "Remove state : state group '$this->objectStateGroupIdentifier' not found." );
+            return;
+        }
+        $this->objectStateGroup->remove( );
+        $this->output->notice( "Remove state group : state group '$this->objectStateGroupIdentifier' removed." );
+    }
 
+    protected function fillStateWithParams( $state, $params ) {
+        foreach( $params as $key => $value ) {
+            if( $state->hasAttribute( $key ) ) {
+                $state->setAttribute( $key, $value );
+            } else {
+                $translation = $state->translationByLocale( $key );
+                if( $translation instanceof eZContentObjectStateLanguage ) {
+                    if( is_array( $value ) ) {
+                        if( isset( $value['name'] ) ) {
+                            $translation->setAttribute( 'name', $value['name'] );
+                        }
+                        if( isset( $value['description'] ) ) {
+                            $translation->setAttribute( 'description', $value['description'] );
+                        }
+                    } else {
+                        $translation->setAttribute( 'name', $value );
+                    }
+                } else {
+                    $this->output->warning( "fill state with params : attribute or translation '$key' not found." );
+                }
+            }
+        }
+        return $state;
     }
 
 }
---
+++ migrate/owmigrationobjectstateexemple.php
@@ -0,0 +1,43 @@
+<?php
+
+class OWMigrationObjectStateExemple extends OWMigration {
+
+    public function up( ) {
+        $migration = new OWMigrationObjectState( );
+        $migration->startMigrationOn( 'my_state' );
+        $migration->createIfNotExists( );
+        $migration->update( array(
+            'fre-FR' => array( 'name' => 'Mon état' ),
+            'eng-GB' => array( 'description' => 'My super state' ),
+            'xxx-YY' => array( 'description' => 'My super state' )
+        ) );
+        $migration->addState( 'my_state_1' );
+        $migration->addState( 'my_state_2', array(
+            'fre-FR' => 'Toto',
+            'eng-GB' => 'Titi'
+        ) );
+        $migration->updateState( 'my_state_1', array(
+            'fre-FR' => 'Tutu',
+            'eng-GB' => array(
+                'name' => 'Tata',
+                'description' => 'Super Tata'
+            )
+        ) );
+        $migration->updateState( 'my_state_3', array(
+            'fre-FR' => 'Tutu',
+            'eng-GB' => array(
+                'name' => 'Tata',
+                'description' => 'Super Tata'
+            )
+        ) );
+        $migration->removeState( 'my_state_1' );
+        $migration->end( );
+    }
+
+    public function down( ) {
+        $migration = new OWMigrationObjectState( );
+        $migration->startMigrationOn( 'my_state' );
+        $migration->removeObjectStateGroup( );
+    }
+
+}
