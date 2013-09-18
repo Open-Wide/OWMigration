8caf81559618ad3cc915eb4a74489d0c119a7118
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2013-09-18 09:25:08 +0200

    History first

--- settings/menu.ini.append.php
+++ settings/menu.ini.append.php
@@ -11,7 +11,7 @@ NavigationPartIdentifier=owmigration
 Name=Migration
 Tooltip=Génération migration code
 URL[]
-URL[default]=owmigration/classes
+URL[default]=owmigration/history
 Enabled[]
 Enabled[default]=true
 Enabled[browse]=false
@@ -25,9 +25,9 @@ PolicyList[]=owmigration/read
 
 [Leftmenu_owmigration]
 Name=OW Migration
+Links[history]=owmigration/history
 Links[content_class]=owmigration/classes
 Links[role]=owmigration/roles
-Links[history]=owmigration/history
 LinkNames[]
 
 */ ?>
