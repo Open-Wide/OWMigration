f75d04574bab83694c402a3d59e14d01aee332a4
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2013-09-20 15:35:14 +0200

    Update table structure

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -4,3 +4,12 @@ CREATE TABLE `owmigration` (
   `date` datetime NOT NULL,                                                                                                                                                                                                                        
   `log` longtext COLLATE utf8_unicode_ci                                                                                                                                                                                                           
 );
+
+TRUNCATE owmigration;
+ALTER TABLE owmigration ADD COLUMN extension varchar(100) NOT NULL;
+ALTER TABLE owmigration ADD COLUMN version varchar(3) NOT NULL;
+ALTER TABLE owmigration ADD COLUMN status varchar(10) NOT NULL;
+ALTER TABLE owmigration DROP COLUMN class;
+ALTER TABLE owmigration DROP COLUMN date;
+ALTER TABLE owmigration DROP COLUMN log;
+ALTER TABLE owmigration DROP COLUMN method;
