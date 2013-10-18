25ab36961711f555ffce67af882884d312c9f517
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2013-10-18 14:03:53 +0200

    Add new exemple

--- migrations/007_owmigration_stategroup.php
+++ migrations/007_owmigration_stategroup.php
@@ -2,22 +2,27 @@
 
 class OWMigration_007_TestStateGroupObjectState {
     public function up( ) {
-        $migration = new OWMigrationObjectState( );
-        $migration->startMigrationOn( 'test_state_group_2' );
+        $migration = new OWMigrationStateGroup( );
+        $migration->startMigrationOn( 'test_state_group_1' );
         $migration->createIfNotExists( );
         $migration->update( array(
             'fre-FR' => array( 'name' => 'Groupe de test' ),
             'default_language_id' => 'fre-FR',
         ) );
 
-        $migration->addState( 'test_etat_1', array( 'fre-FR' => array( 'name' => 'Test état 1' ), ) );
+        $migration->addState( 'test_etat_1', array( 'fre-FR' => array( 'name' => 'Test état 1' ) ) );
+        $migration->addState( 'test_etat_2' );
+        $migration->addState( 'test_etat_3' );
+        $migration->updateState( 'test_etat_1', array( 'fre-FR' => array( 'name' => 'Test état 1 (bis)' ) ) );
+        $migration->removeState( 'test_etat_2' );
+        $migration->addState( 'test_etat_4', array( 'priority' => 1 ) );
         $migration->end( );
     }
 
     public function down( ) {
-        $migration = new OWMigrationObjectState( );
-        $migration->startMigrationOn( 'test_state_group' );
-        $migration->removeObjectState( );
+        $migration = new OWMigrationStateGroup( );
+        $migration->startMigrationOn( 'test_state_group_1' );
+        $migration->removeStateGroup( );
     }
 
 }
