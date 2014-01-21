1258ae5485004613a4827593d2856c3037862ceb
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2014-01-21 12:16:59 +0100

    Add manual step

--- classes/migration/owmigrationbase.php
+++ classes/migration/owmigrationbase.php
@@ -2,14 +2,32 @@
 
 abstract class OWMigrationBase {
 
-    protected $output;
-    protected $db;
+	protected $output;
+	protected $db;
 
-    public function __construct( ) {
-        $this->db = eZDB::instance( );
-    }
-    
-    abstract function startMigrationOn( $param );
-    abstract function end( );
+	public function __construct() {
+		$this->db = eZDB::instance();
+	}
+
+	abstract function startMigrationOn( $param );
+
+	abstract function end();
+
+	public function manualStep( $action, $description ) {
+		OWScriptLogger::writeNotice( "=== $action ===" . PHP_EOL . $description . PHP_EOL . "-> You can add a comment and press enter to continue. If you want to report a warning or an error, start you comment by 'warn:' or 'error:'.", 'manual_step' );
+		$fp = fopen( "php://stdin", "r" );
+		$result = trim( fgets( $fp ) );
+		if ( empty( $result ) ) {
+			OWScriptLogger::logNotice( $action, 'manual_step' );
+		} elseif ( strpos( strtolower( $result ), 'warn:' ) === 0 ) {
+			$result = trim( substr( $result, 5 ) );
+			OWScriptLogger::logWarning( $action . PHP_EOL . "Comment: $result", 'manual_step' );
+		} elseif ( strpos( strtolower( $result ), 'error:' ) === 0 ) {
+			$result = trim( substr( $result, 6 ) );
+			OWScriptLogger::logError( $action . PHP_EOL . "Comment: $result", 'manual_step' );
+		} else {
+			OWScriptLogger::logNotice( $action . PHP_EOL . "Comment: $result", 'manual_step' );
+		}
+	}
 
 }
