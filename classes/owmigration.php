bf9eff2cc9c55ed8b0c1e7ac3dc16d0e1750949e
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2014-04-02 14:47:58 +0200

    Execute queries one by one

--- classes/owmigration.php
+++ classes/owmigration.php
@@ -75,7 +75,24 @@ class OWMigration {
             // but eZDbSchemaChecker::diff does not know how to re-localize the generated sql
             $dbSchema->transformSchema( $originalSchema, true );
             $differences = eZDbSchemaChecker::diff( $dbSchema->schema( array( 'format' => 'local', 'force_autoincrement_rebuild' => true ) ), $originalSchema );
-            $sqlDiff = $dbSchema->generateUpgradeFile( $differences );
+            $queries = array();
+            if ( isset( $differences['table_changes'] ) ) {
+                foreach ( $differences['table_changes'] as $table => $table_diff ) {
+                    $queries[] = $dbSchema->generateUpgradeFile( array( 'table_changes' => array( $table => $table_diff ) ) );
+                }
+            }
+            if ( isset( $differences['new_tables'] ) ) {
+                foreach ( $differences['new_tables'] as $table => $table_def ) {
+                    $queries[] = $dbSchema->generateUpgradeFile( array( 'new_tables' => array( $table => $table_def ) ) );
+                }
+            }
+            if ( isset( $differences['removed_tables'] ) ) {
+                foreach ( $differences['removed_tables'] as $table => $table_def ) {
+                    $queries[] = $dbSchema->generateUpgradeFile( array( 'removed_tables' => array( $table => $table_def ) ) );
+                }
+            }
+            
+            $sqlDiff = implode(PHP_EOL, $queries);
 
             if ( strlen( $sqlDiff ) == 0 ) {
                 OWScriptLogger::logNotice( "The database schema is up to date.", 'migrate' );
@@ -86,7 +103,9 @@ class OWMigration {
                 while ( $badAnswer ) {
                     $result = trim( fgets( $fp ) );
                     if ( strtolower( $result ) == 'y' ) {
-                        $db->query( $sqlDiff );
+                        foreach( $queries as $query ) {
+                            $db->query( $query );
+                        }
                         OWScriptLogger::logNotice( "The database schema has been updated.", 'migrate' );
                         $badAnswer = false;
                     } elseif ( strtolower( $result ) == 'n' ) {
