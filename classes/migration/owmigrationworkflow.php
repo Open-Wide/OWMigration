4e85dfba0ce1e3d0ceb381c327d583944941ffd7
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2014-07-17 15:12:40 +0200

    Fix workflow group creation

--- classes/migration/owmigrationworkflow.php
+++ classes/migration/owmigrationworkflow.php
@@ -73,7 +73,7 @@ class OWMigrationWorkflow extends OWMigrationBase {
 				break;
 			}
 		}
-		if ( !isset( $workflowGroup ) && !$workflowGroup ) {
+		if ( !isset( $workflowGroup ) || !$workflowGroup ) {
 			OWScriptLogger::logNotice( "Workflow group '$groupName' created.", __FUNCTION__ );
 			$user = eZUser::currentUser();
 			$workflowGroup = eZWorkflowGroup::create( $user->attribute( 'contentobject_id' ) );
