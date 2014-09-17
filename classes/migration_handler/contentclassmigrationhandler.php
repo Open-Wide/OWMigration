2aa41d1b7d146cd43ab3879a04c54ac79e13043d
Madeline VEYRENC <madeline.veyrenc@openwide.fr>
2014-09-17 15:25:54 +0200

    Add missing file

---
+++ classes/migration_handler/contentclassmigrationhandler.php
@@ -0,0 +1,50 @@
+<?php
+
+class ContentClassMigrationHandler {
+
+    static public function toArray( eZContentClass $contentClass ) {
+        $contentClassArray = array();
+        foreach ( $contentClass->attributes() as $attributeIdentifier ) {
+            $attributeValue = $contentClass->attribute( $attributeIdentifier );
+            switch ( $attributeIdentifier ) {
+                case 'name' :
+                case 'description' :
+                    $nameList = $contentClass->attribute( $attributeIdentifier . 'List' );
+                    $nameListValue = OWMigrationTools::cleanupNameList( $nameList );
+                    if ( !empty( $nameListValue ) ) {
+                        $contentClassArray[$attributeIdentifier] = $nameListValue;
+                    }
+                    break;
+                case 'contentobject_name' :
+                case 'url_alias_name' :
+                    if ( $attributeValue != '' ) {
+                        $contentClassArray[$attributeIdentifier] = $attributeValue;
+                    }
+                    break;
+                case 'is_container' :
+                case 'always_available' :
+                    if ( $attributeValue == TRUE ) {
+                        $contentClassArray[$attributeIdentifier] = TRUE;
+                    }
+                    break;
+                case 'sort_field' :
+                    if ( $attributeValue != 1 ) {
+                        $contentClassArray[$attributeIdentifier] = $attributeValue;
+                    }
+                    break;
+                case 'sort_order' :
+                    if ( $attributeValue == FALSE ) {
+                        $contentClassArray[$attributeIdentifier] = FALSE;
+                    }
+                    break;
+                default :
+                    break;
+            }
+        }
+        return $contentClassArray;
+    }
+
+    function fromArray( eZContentClass $contentClass, array $options ) {
+    }
+
+}
